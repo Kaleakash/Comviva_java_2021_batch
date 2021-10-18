26905e7e11aedb8e36f5d2da916e1c386be0ce61
akash <akash300383@gmail.com>
2021-10-18 17:54:04 +0530

    done

---
+++ Phase 3/Spring Framework/DIUsingXML/src/main/java/com/Employee.java
@@ -0,0 +1,26 @@
+package com;
+
+public class Employee {
+private int id;
+private String name;
+private float salary;
+	Employee() {
+		System.out.println("Object created...");
+	}
+	
+	public Employee(int id, String name, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public void display() {
+		System.out.println("This is employee class method");
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+}
--- Web Application/MVC_Application/src/main/webapp/index.jsp
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<h2>Student Management System</h2>
-<!--  %@include file="insertStudentRecord.jsp" % -->
-
-<%@include file="SignIn.jsp"  %>
-
-</body>
-</html>
