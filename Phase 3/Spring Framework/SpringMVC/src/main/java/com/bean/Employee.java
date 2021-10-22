7507271cecc55b44dccfef94c0774a6060ed83fb
akash <akash300383@gmail.com>
2021-10-22 17:47:40 +0530

    done

--- Phase 3/Spring Framework/SpringMVC/src/main/java/com/bean/Employee.java
+++ Phase 3/Spring Framework/SpringMVC/src/main/java/com/bean/Employee.java
@@ -1,6 +1,11 @@
 package com.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
+@Id				// PK 
 private int id;
 private String name;
 private float salary;
